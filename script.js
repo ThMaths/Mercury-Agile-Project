9540e015e28f98523b11c7cfde925b302a2a2930
megane <susini.megane04@gmail.com>
2024-04-30 16:02:17 +0200

    changes

--- script.js
+++ script.js
@@ -25,3 +25,53 @@ function addTask() {
     }
   }
 }
+
+function addTask2() {
+  var taskInput = document.getElementById("taskInput");
+  var taskText = taskInput.value.trim();
+
+  if (taskText !== "") {
+      taskList1 = document.getElementById("importantUrgentTasks");
+      taskList2 = document.getElementById("notImportantNotUrgentTasks");
+      taskList3 = document.getElementById("importantNotUrgentTasks");
+      taskList4 = document.getElementById("notImportantUrgentTasks");
+
+      allTaskLists = [taskList1, taskList2, taskList3, taskList4];
+      
+      var taskList = null;
+      for (var i=0; i<=allTaskLists.length; i++) {
+        if(taskList == null) {
+          taskList = allTaskLists[i];
+        }
+      }
+
+      var newCheckBox = document.createElement('input');
+      newCheckBox.type = 'checkbox';
+
+      var label = document.createElement("label");
+      label.textContent = taskText;
+
+      var div = document.createElement("div");
+      div.appendChild(newCheckBox);
+      div.appendChild(label);
+      taskList.appendChild(div);
+
+      taskInput.value = "";
+  }
+}
+
+function ImportantAndUrgent() {
+  window.location.href = "/important_and_urgent.html";
+}
+
+function ImportantNotUrgent() {
+  window.location.href = "/important_not_urgent.html";
+}
+
+function NotImportantButUrgent() {
+  window.location.href = "/not_important_but_urgent.html";
+}
+
+function NotImportantNotUrgent() {
+  window.location.href = "/not_important_not_urgent.html";
+}

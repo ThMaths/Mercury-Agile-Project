7512d133f8ff304bfedd31638194437cdb542fb5
ThMaths <mathiasthomas@outlook.fr>
2024-04-30 11:41:15 +0200

    change choice

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function addTask() {
   var taskInput = document.getElementById("taskInput");
   var taskText = taskInput.value.trim();
+  var prioritySelect = document.getElementById("prioritySelect");
+  var priority = prioritySelect.value;
+
   if (taskText !== "") {
-    var priority = prompt("Entrez la priorité de la tâche (1: Important et Urgent, 2: Important mais Pas Urgent, 3: Pas Important mais Urgent, 4: Pas Important et Pas Urgent)");
-    if (priority === "1" || priority === "2" || priority === "3" || priority === "4") {
+    if (priority === "") {
+      alert("Erreur : Veuillez sélectionner une priorité.");
+    } else {
       var taskList = document.getElementById("notImportantNotUrgentTasks"); // Par défaut
       if (priority === "1") {
         taskList = document.getElementById("importantUrgentTasks");
@@ -12,12 +16,12 @@ function addTask() {
       } else if (priority === "3") {
         taskList = document.getElementById("notImportantUrgentTasks");
       }
+
       var li = document.createElement("li");
       li.textContent = taskText;
       taskList.appendChild(li);
       taskInput.value = "";
-    } else {
-      alert("Erreur : Veuillez entrer une priorité valide (1, 2, 3 ou 4).");
+      prioritySelect.value = "";
     }
   }
 }

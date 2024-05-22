9fbf0baf2b220c1d0a934b3a1a070cbb56f730c6
Mégane <susini.megane04@gmail.com>
2024-05-22 16:15:14 +0200

    Update connection_system.php

--- connection_system.php
+++ connection_system.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <form class="d-flex align-items-center flex-column">
+    <form class="d-flex vh-100 align-items-center justify-content-center flex-column">
         <div class="form-group">
             <label for="">Identifiant</label>
             <input class="form-control" id="">
@@ -18,6 +18,7 @@
         </div>
         <button type="submit" class="btn btn-primary">Se connecter</button>
     </form>
+        
     <!-- Bootstrap JS and dependencies -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>

d92b751ca494a02fd835cbe752e7c8936e595ad4
Mégane <susini.megane04@gmail.com>
2024-05-22 14:29:06 +0200

    Update connection_system.php

--- connection_system.php
+++ connection_system.php
@@ -8,20 +8,15 @@
 </head>
 <body>
     <form>
-        <!-- Email input -->
-        <div data-mdb-input-init class="form-outline mb-4">
-            <input type="email" id="form2Example1" class="form-control" />
-            <label class="form-label" for="form2Example1">Email address</label>
+        <div class="form-group">
+            <label for="">Identifiant</label>
+            <input class="form-control" id="">
         </div>
-
-        <!-- Password input -->
-        <div data-mdb-input-init class="form-outline mb-4">
-            <input type="password" id="form2Example2" class="form-control" />
-            <label class="form-label" for="form2Example2">Password</label>
+        <div class="form-group">
+            <label for="">Mot de passe</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
         </div>
-
-        <!-- Submit button -->
-        <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">Sign in</button>
+        <button type="submit" class="btn btn-primary">Se connecter</button>
     </form>
     <!-- Bootstrap JS and dependencies -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
